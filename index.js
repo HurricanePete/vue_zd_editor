7efc80297db015ef1defc17b2d11b08ef8693bb7
HurricanePete <zachary@smartly.io>
2018-11-01 16:16:22 -0400

    initial commit without examples

---
+++ index.js
@@ -0,0 +1,95 @@
+const app8 = new Vue({
+    el: '#app-8',
+    data: {
+        totalArticles: exampleArray.length,
+        currentArticleIndex: 0,
+        articleEdited: false,
+        filteredArticles: exampleArray,
+        editedArticles: [],
+        searchTerm: null
+    },
+    methods: {
+        nextArticle: function() {
+            if(this.currentArticleIndex === (this.totalArticles - 1)) {
+                return
+            }
+            this.currentArticleIndex++;
+        },
+        prevArticle: function() {
+            if(this.currentArticleIndex === 0) {
+                return
+            }
+            this.currentArticleIndex--;
+        }
+    }
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+})
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+Vue.component('todo-item', {
+    // The todo-item component now accepts a
+    // "prop", which is like a custom attribute.
+    // This prop is called todo.
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+    }
+})
